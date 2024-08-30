af503501a5179799165ac0646b6f518b045a952c
TAsenova <tasenova@mentormate.com>
2024-08-30 15:32:34 +0300

    using reactive

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="app">
-    <p>{{ state.name }} - {{ state.age }}</p>
+    <p>{{ name }} - {{ age }}</p>
     <button @click="changeName('Another Name')">Change name</button>
     <button @click="changeAge('40')">Change Age</button>
   </div>
 </template>
 
-<script setup lang="ts">
-import { reactive } from "vue";
+<script lang="ts">
+import { reactive, defineComponent, toRefs } from "vue";
 
 type State = {
   name: string;
   age: number | string;
 };
 
-const state: State = reactive({
-  name: "A Name",
-  age: 27,
-});
+// const state: State = reactive({
 
-const changeAge = (newAge: number | string) => {
-  state.age = newAge;
-};
-const changeName = (newName: string) => {
-  state.name = newName;
-};
+// });
+
+// const changeAge = (newAge: number | string) => {
+//   state.age = newAge;
+// };
+// const changeName = (newName: string) => {
+//   state.name = newName;
+// };
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const state: State = reactive({
+      name: "A Name",
+      age: 27,
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
+  methods: {
+    changeAge(newAge: number | string) {
+      this.age = newAge;
+    },
+    changeName(newName: string) {
+      this.name = newName;
+    },
+  },
+});
 </script>
 
 <style></style>
