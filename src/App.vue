167082224c1a219b24f549b4adf412782088047b
TAsenova <tasenova@mentormate.com>
2024-08-30 15:24:51 +0300

    Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
+    <p>{{ state.name }} - {{ state.age }}</p>
     <button @click="changeName('Another Name')">Change name</button>
     <button @click="changeAge('40')">Change Age</button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { reactive } from "vue";
 
-export default defineComponent({
-  name: "App",
-  components: {},
-  data() {
-    return {
-      name: "A Name",
-      age: 27 as string | number,
-    };
-  },
-  methods: {
-    changeName(newName: string) {
-      this.name = newName;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-    },
-  },
+type State = {
+  name: string;
+  age: number | string;
+};
+
+const state: State = reactive({
+  name: "A Name",
+  age: 27,
 });
+
+const changeAge = (newAge: number | string) => {
+  state.age = newAge;
+};
+const changeName = (newName: string) => {
+  state.name = newName;
+};
 </script>
 
 <style></style>
