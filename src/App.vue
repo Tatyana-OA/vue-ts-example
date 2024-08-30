cb70f0bf6cbd28ae8025cb4a550bc2e125882a81
TAsenova <tasenova@mentormate.com>
2024-08-30 15:17:59 +0300

    Simple ts example within vue compoennt

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Hello, World!</h1>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Another Name')">Change name</button>
+    <button @click="changeAge('40')">Change Age</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +12,20 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
+  data() {
+    return {
+      name: "A Name",
+      age: 27 as string | number,
+    };
+  },
+  methods: {
+    changeName(newName: string) {
+      this.name = newName;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+    },
+  },
 });
 </script>
 
